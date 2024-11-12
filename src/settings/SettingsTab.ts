6a80cc1c496ec28f362b1107735008f26697dccc
Joseph Platta <jwplatta@gmail.com>
2024-11-11 22:25:41 -0600

    create settings tab class

---
+++ src/settings/SettingsTab.ts
@@ -0,0 +1,25 @@
+export class SettingsTab extends PluginSettingTab {
+	plugin: AutoJournal;
+
+	constructor(app: App, plugin: AutoJournal) {
+		super(app, plugin);
+		this.plugin = plugin;
+	}
+
+	display(): void {
+		const {containerEl} = this;
+
+		containerEl.empty();
+
+		new Setting(containerEl)
+			.setName('Setting #1')
+			.setDesc('It\'s a secret')
+			.addText(text => text
+				.setPlaceholder('Enter your secret')
+				.setValue(this.plugin.settings.mySetting)
+				.onChange(async (value) => {
+					this.plugin.settings.mySetting = value;
+					await this.plugin.saveSettings();
+				}));
+	}
+}
