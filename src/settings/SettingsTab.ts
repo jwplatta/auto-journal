e3f2298ce929b0d6ff0fac5efc35d7240128f119
Joseph Platta <jwplatta@gmail.com>
2024-11-12 23:17:44 -0600

    add the gitignore settings to the settings tab

--- src/settings/SettingsTab.ts
+++ src/settings/SettingsTab.ts
@@ -1,13 +1,16 @@
 import { App, PluginSettingTab, Setting } from 'obsidian';
 import { openAISettings } from '@/src/settings/OpenAISettings';
 import { ollamaSettings } from '@/src/settings/OllamaSettings';
+import { gitignoreSettings } from '@/src/settings/GitignoreSettings';
 import AutoJournal from '@/main';
 
 export class SettingsTab extends PluginSettingTab {
+  app: App;
 	plugin: AutoJournal;
 
 	constructor(app: App, plugin: AutoJournal) {
 		super(app, plugin);
+    this.app
 		this.plugin = plugin;
 	}
 
@@ -16,11 +19,30 @@ export class SettingsTab extends PluginSettingTab {
     containerEl.empty();
 		containerEl.createEl('h1', { text: 'Auto Journal Settings' });
 
-		addHorizontalRule(containerEl);
+
+    // new Setting(containerEl)
+    //   .setName('Exclude Files and Folders')
+    //   .setDesc('Exclude specific files and folders')
+    //   .addTextArea(text => {
+    //     text
+    //       .setPlaceholder('file1.md, /Notes')
+    //       .setValue(this.plugin.settings.fileExclusions.join(', '))
+    //       .onChange(async (value) => {
+    //         this.plugin.settings.fileExclusions = value.split(',').map((v: string) => v.trim());
+    //         await this.plugin.saveSettings();
+    //       })
+    //       .then((cb) => {
+    //         cb.inputEl.style.width = '100%';
+    //         cb.inputEl.style.resize = 'vertical';
+    //       });
+    //   });
+
+    gitignoreSettings(containerEl, this.plugin, this.app, this);
+
     // NOTE: folder for journal entries
     // NOTE: schedule for writing the journal entries
-    // NOTE: configure them models
 
+    // NOTE: configure them models
     addHorizontalRule(containerEl);
     openAISettings(containerEl, this.plugin, this);
 
